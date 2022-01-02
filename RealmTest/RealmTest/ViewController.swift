1221c9c7fd0acddcf53d37035544a2aa02626bb5
kimxwan <kimxwan0319@naver.com>
2022-01-02 16:08:21 +0900

    ⚙️ :: project init

---
+++ RealmTest/RealmTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmTest
+//
+//  Created by 김수완 on 2022/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
